be31799e5451b9f6a3b039dc5a19f554d789049a
Felipe Lenz <134728459+Fel1peZ@users.noreply.github.com>
2025-02-10 15:05:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -22,3 +22,21 @@ let toggleMenu = document.getElementById('toggleMenu');
 
 
            });
+
+document.addEventListener("DOMContentLoaded", function () {
+const profileIcon = document.getElementById("profileIcon");
+const profileMenu = document.getElementById("profileMenu");
+
+// Abrir ou fechar o menu ao clicar no ícone
+profileIcon.addEventListener("click", function (event) {
+    profileMenu.classList.toggle("show");
+    event.stopPropagation(); // Impede que o clique feche o menu imediatamente
+});
+
+// Fechar o menu ao clicar fora dele
+document.addEventListener("click", function (event) {
+    if (!profileMenu.contains(event.target) && event.target !== profileIcon) {
+        profileMenu.classList.remove("show");
+    }
+});
+});
